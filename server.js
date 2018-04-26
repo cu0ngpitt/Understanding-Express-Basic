63f91288c19371652bd0e2a2dc6146f4fde78a0c
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-04-26 15:36:43 -0500

    File Upload

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+var upload = multer({ dest: '/tmp/'});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.post('/file_upload', function (req, res) {
+   console.log(req.files.file.name);
+   console.log(req.files.file.path);
+   console.log(req.files.file.type);
+   var file = __dirname + "/" + req.files.file.name;
+
+   fs.readFile( req.files.file.path, function (err, data) {
+      fs.writeFile(file, data, function (err) {
+         if( err ){
+            console.log( err );
+            }else{
+               response = {
+                  message:'File uploaded successfully',
+                  filename:req.files.file.name
+               };
+            }
+         console.log( response );
+         res.end( JSON.stringify( response ) );
+      });
+   });
 })
 
 var server = app.listen(8081, function () {
