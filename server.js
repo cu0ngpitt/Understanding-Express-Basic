fbf83f2cc9048bed795e3bff3db6e32e02c6704b
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-04-26 14:59:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.htm" );
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
 })
 
 app.get('/process_get', function (req, res) {
